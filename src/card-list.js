7d24697372f9c67a1e0e5eb3a95cfed7518a6df6
Pandaa <96748487+Pandaalifter@users.noreply.github.com>
2023-02-27 12:15:34 -0800

    florb

--- src/card-list.js
+++ src/card-list.js
@@ -14,38 +14,10 @@ class CardList extends LitElement {
     :host {
       display: inline-block;
     }
-
-    main {
-      flex-grow: 1;
-    }
-
-    .logo {
-      margin-top: 36px;
-      animation: app-logo-spin infinite 20s linear;
-    }
-
-    @keyframes app-logo-spin {
-      from {
-        transform: rotate(0deg);
-      }
-      to {
-        transform: rotate(360deg);
-      }
-    }
-
-    .app-footer {
-      font-size: calc(12px + 0.5vmin);
-      align-items: center;
-    }
-
-    .app-footer a {
-      margin-left: 5px;
-    }
   `;
 
   constructor() {
     super();
-    this.header = 'My app';
   }
 
   render() {
@@ -65,7 +37,7 @@ class CardList extends LitElement {
       <professor-card2>
         <p>Professor Giacobe may look like a simple man but perceptions can be misleading. Underneath the facade of a simple college of IST professor lies the holiness of an <strong>IST GOD</strong>!</p>
       </professor-card2>
-      
+
       <professor-card>
         <ul>
           <li>Age: Unknowable</li>
